e01c990aec817e6e346148aaadf70188df926220
MohamedMoataz266 <83556372+MohamedMoataz266@users.noreply.github.com>
2021-05-09 22:07:18 +0200

    Add files via upload

---
+++ Shool Management Sysrem/studentHome.php
@@ -0,0 +1,20 @@
+<?php
+include "studentmenu.php";
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Admin</title>
+  <style>
+<?php include "Styles/sa.css"; ?>
+body{
+		background-image: url('Images/banner_img.png');;
+	}
+</style>
+</head>
+<body>
+	
+
+</body>
+
+</html>
