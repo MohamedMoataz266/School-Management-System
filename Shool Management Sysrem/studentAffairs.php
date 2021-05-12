b78afdc355447fc8e5c77b6c58387bcd25a91f3b
YoussifAssem <76666401+YoussifAssem@users.noreply.github.com>
2021-05-12 17:38:20 +0200

    Add files via upload

--- Shool Management Sysrem/studentAffairs.php
+++ Shool Management Sysrem/studentAffairs.php
@@ -6,27 +6,27 @@ public function addStudent($fiN, $sN, $tN, $fN, $nN, $bD, $g, $n, $r, $p, $m, $a
      $st = new Student();
      $st->insertStudent($fiN, $sN, $tN, $fN, $nN, $bD, $g, $n, $r, $p, $m, $a, $ph, $f, $c, $rN);
  }
- private function checkStudent($name, $nationalNumber){
+ private function checkStudent($id){
     include "dB.php";
     $flag = true;
-    $sql = mysqli_query($conn, "SELECT name, nationalNumber FROM Students WHERE name='".$name."' AND nationalNumber= '".$nationalNumber."'");
+    $sql = mysqli_query($conn, "SELECT ID FROM Students WHERE ID='".$id."'");
     if(mysqli_num_rows($sql) > 0){
       return $flag; 
     }
     $flag = false;
     return $flag;
  }
- public function removeStudent($name, $nationalNumber){
+ public function removeStudent($id){
     include "dB.php";
-    if($this->checkStudent($name, $nationalNumber)){
-        mysqli_query($conn, "DELETE FROM Students WHERE nationalNumber='$nationalNumber' AND name='$name'");
+    if($this->checkStudent($id)){
+        mysqli_query($conn, "DELETE FROM Students WHERE ID='$id'");
         echo '<script>alert("Done, Student Removed Successfully")</script>';
-           return;
-    }
-    else{
-        echo '<script>alert("Error, Data Is Not True")</script>';
         return;
     }
-}   
+    else{
+        echo '<script>alert("Error, Student Not Removed")</script>';
+        return;
+    }
+  }   
 }
 ?>
