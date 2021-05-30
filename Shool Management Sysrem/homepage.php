83e7d17f72bdc9a9bec400baafbb918ef52e4369
kareem644 <76753975+kareem644@users.noreply.github.com>
2021-05-30 23:33:55 +0200

    homepage
    new php and css

---
+++ Shool Management Sysrem/homepage.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<!--Code by Divinector (www.divinectorweb.com)-->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>How to Create responsive Homepage</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap" rel="stylesheet">
+</head>
+<style>
+    <?php 
+    include 'Styles/homepage.css';
+    ?>
+   </style>
+<body>
+    <header>
+    <div class="wrapper">
+        <div class="logo">
+            <img src="Images/amoun.jpg" alt="">
+        </div>
+<ul class="nav-area">
+<li><a href="homepage.php">Home</a></li>
+<li><a href="Registration.php">Sign Up</a></li>
+<li><a href="logIn.php">Log In</a></li>
+<li><a href="normalLogIn.php">Already a user ?</a></li>
+</ul>
+</div>
+<div class="welcome-text">
+<h1> <span>Amoun Language School</span></h1>
+    </div>
+</header>
+ 
+            
+                        <div class="full">
+                            <div class="picture">
+                                
+                             <img src="Images/unnamed.jpg" alt="#" />
+                             
+                         </div>
+                          <p>The stages prepare students from kindergarten to the end of high school.
+                          The joint school is keen on developing the Egyptian personality morally, morally, scientifically and psychologically.
+                            The school is interested in breaking out of stereotypes through its constant quest for renewal by introducing everything new in the educational process.
+                         School activity has a big side in the mind and conscience of Amun School, as it always works to encourage multiple talents in various activities.</p>
+                        </div>
+                        <br><br>
+                         <div class="secondfull">
+                            <div class="secondpicture">
+                                
+                             <img src="Images/cbdc54feb9a9d3b062b99fd0b400ba5b.jpg" alt="#" />
+                             
+                         </div>
+                          <p> Address: 31 ش يحيى ابراهيم، Mohammed Mazhar, Zamalek, Giza Governorate</p>
+                            <a class="hvr-radial-out button-theme" href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.google.com%2Fmaps%2Fdir%2F%3Fapi%3D1%26destination%3D30.061160272663%252C31.223843795102%26fbclid%3DIwAR2sA6smgGHOwQ8E-H_ApMJ810RMrxs2R5sZT8ziNlk80-FVki4JImbdLSk&h=AT3K02ogMd0MhyWv_tFLesLPa5yiiqz8xIdgAHwJScw4g6k81z3YQ7qcohRbHYkdAYZNkrN_GdSJjCEen7OrsN0zin6ZcS0HRORiP5MQs0jwjzGIbawTtr0bVQ8ULXylUAFoMg">get the location</a>
+                        </div>
+                 
+</body>
+</html>
