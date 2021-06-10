96c4b869be6722d50a4f6ae76e299267dd327d9f
YoussifAssem <youssifassem1815221@miuegypt.edu.eg>
2021-06-10 19:27:46 +0200

    Crazy update
    Update homemenu to become generic menu

--- Shool Management Sysrem/homemenu.php
+++ Shool Management Sysrem/homemenu.php
@@ -1,27 +1,27 @@
-<link rel="stylesheet" type="text/css" href="Styles/homepage.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-<script src='Js/menu.js'></script>
-<body onload="homeMenu()">
-<nav>
-    <input id="nav-toggle" type="checkbox">
-    <div class="logo"><strong>Welcome to Amoun</strong>&nbsp
+    <link rel="stylesheet" type="text/css" href="Styles/homepage.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src='Js/menu.js'></script>
+    <body onload="homeMenu()">
+    <nav>
+        <input id="nav-toggle" type="checkbox">
+        <div class="logo"><strong>Welcome to Amoun</strong>&nbsp
 
-    <div class="logo">
-    <img src = "Images/amoun.jpeg">
+        <div class="logo">
+        <img src = "Images/amoun.jpeg">
+        </div>
     </div>
-</div>
-    <ul class="links">
-        <li><a class='home'></a></li>
-        <li><a class='atr1'></a></li>
-        <li><a class='atr2'></a></li>
-    </ul>
-    <label for="nav-toggle" class="icon-burger">
-        <div class="line"></div>
-        
-    </label>
-</nav>
+        <ul class="links">
+            <li><a class='home'></a></li>
+            <li><a class='atr1'></a></li>
+            <li><a class='atr2'></a></li>
+        </ul>
+        <label for="nav-toggle" class="icon-burger">
+            <div class="line"></div>
+            
+        </label>
+    </nav>
 
-<div class="container2">
-</div>
-</body>
+    <div class="container2">
+    </div>
+    </body>
 
