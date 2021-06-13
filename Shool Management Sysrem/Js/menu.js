9e6b32fc90f4e8f65f447b55e592d0c07cfd7102
kareem644 <76753975+kareem644@users.noreply.github.com>
2021-06-14 00:17:46 +0200

    video

--- Shool Management Sysrem/Js/menu.js
+++ Shool Management Sysrem/Js/menu.js
@@ -22,7 +22,7 @@ function teacherMenu(){
         $(".atr1").replaceWith('<a href="addQuestions.php">Add Questions</a>');
         $(".atr2").replaceWith('<a href="viewstudents.php">View Students</a>');
         $(".atr3").replaceWith('<a href="">Add Grades</a>');
-        $(".atr4").replaceWith('<a href="addvideocourse.php">Add Video</a>');
+        $(".atr4").replaceWith('<a href="addvideo.php">Add Video</a>');
         $(".atr5").replaceWith('<a href="viewAnswers.php">View Answers</a>');
         $(".atr6").replaceWith('<a href="teacherMessageFrontEnd.php">Send Message</a>');
         $(".atr7").replaceWith('<a href="viewMessageTeacher.php">View Message</a>');
@@ -32,7 +32,7 @@ function teacherMenu(){
 function videoMenu(){
     $.get("homemenu.php", function(data){
         $(".home").replaceWith('<a href="teacherFrontEnd.php">Home</a>');
-        $(".atr1").replaceWith('<a href="addvideocourse.php">Add Video</a>');
+        $(".atr1").replaceWith('<a href="addvideo.php">Add Video</a>');
         $(".atr2").replaceWith('<a href="deletevideocourse.php">Delete Video</a>');
         $(".atr3").replaceWith('<a href="">Update Video</a>');
     });
